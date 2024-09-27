de0f74a698bd62fd0f90ee7b63c49784be0d8901
lohengrin <oljn22@student.bth.se>
2024-09-27 14:56:08 +0200

    Work on mazerunner select

--- kmom05/maze/client/mazerunner.bash
+++ kmom05/maze/client/mazerunner.bash
@@ -10,6 +10,11 @@
 #
 
 
+
+##
+## VARIABLES DEFINITIONS
+##
+
 # Name of the script
 SCRIPT=$( basename "$0" )
 
@@ -22,14 +27,20 @@ PORT="1337"
 BASE_URL="$HOST:$PORT"
 TO_CSV="?type=csv"
 
-# global var used with 'get-maps' function
+# global vars used with 'get-maps' and get_game_id functions
 declare -a MAPS_AVAILABLE
+GAME_ID=""
 
 # Define color codes
 GREEN="\033[0;32m"
 RED="\033[0;31m"
 NO_COLOR="\033[0m"
 
+
+
+##
+## UTIL FUNCTIONS
+##
 
 #
 # Message to display for usage and help.
@@ -56,8 +67,6 @@ function usage
     printf "%s\\n" "${txt[@]}"
 }
 
-
-
 #
 # message to display when bad usage.
 #
@@ -72,9 +81,9 @@ function badUsage
     [[ -n $message ]] && printf "%s\\n" "$message"
 
     printf "%s\\n" "${txt[@]}"
+
+    exit 1
 }
-
-
 
 #
 # print version.
@@ -88,7 +97,6 @@ function version
     printf "%s\\n" "${txt[@]}"
 }
 
-
 #
 # handle error from curl
 #
@@ -99,15 +107,14 @@ function curl_error
     exit 1
 }
 
-
 #
 # handle response error
 #
 function response_error
 {
     # get status code from header, and full content-body
-    status_code="$(echo $res | head -n 1 | grep -Ewo "[0-9]{3}")"
-    content="$(echo $res | tail -n 1)"
+    status_code="$(echo "$res" | head -n 1 | grep -Ewo "[0-9]{3}")"
+    content="$(echo "$res" | tail -n 1)"
     content="${content//\"{}/}" # remove quotes and curly brackets
 
     echo "Unable to create new game!"
@@ -116,33 +123,10 @@ function response_error
     exit 1
 }
 
-
-#
-# init game, and save id to 'game_id'
-#
-function app-init
-{
-    # request '/' (header and body, silent, include error from curl)
-    url="$BASE_URL/$TO_CSV"
-    if ! res="$(curl -isS "$url")"; then
-        curl_error "$url" "$res"
-    fi
-
-    # get game id from body
-    if ! game_id="$(echo $res | tail -n 1 | grep -Ewo "[0-9]{5}")"; then
-        response_error "$res"
-    fi
-
-    # save game id to file
-    echo "$game_id" > "./game_id"
-    echo "A new game is created with id: $game_id"
-    echo "NEXT STEP: show maps with './$SCRIPT maps'"
-}
-
 #
 # update global array MAPS_AVAILABLE with available maps
 #
-function get-maps
+function get_maps
 {
     # request '/map' (header and body, silent, include error from curl)
     url="$BASE_URL/map$TO_CSV"
@@ -158,24 +142,70 @@ function get-maps
     # append maps to MAPS_AVAILABLE
     for map in $maps; do
         map="${map%.json}"          # remove file extension
-        MAPS_AVAILABLE+=("$map")   # append map
+        MAPS_AVAILABLE+=("$map")    # append map
         (( i++ ))
     done
 }
 
 #
+# update global GAME_ID with id from file 'game_id'. exit with msg if nonexisting.
+#
+function get_game_id
+{
+    if ! id="$(cat "game_id")"; then
+        echo "STEP ONE: start a new game with './$SCRIPT init'"
+        exit 1
+    fi
+
+    GAME_ID="$id"
+}
+
+
+
+##
+## APP FUNCTIONS
+##
+
+#
+# init game, and save id to 'game_id'
+#
+function app_init
+{
+    # request '/' (header and body, silent, include error from curl)
+    url="$BASE_URL/$TO_CSV"
+    if ! res="$(curl -isS "$url")"; then
+        curl_error "$url" "$res"
+    fi
+
+    # get game id from body
+    if ! game_id="$(echo "$res" | tail -n 1 | grep -Ewo "[0-9]{5}")"; then
+        response_error "$res"
+    fi
+
+    # save game id to file
+    echo "$game_id" > "./game_id"
+    echo "A new game is created with id: $game_id"
+    echo "NEXT STEP: show maps with './$SCRIPT maps'"
+}
+
+
+#
 # show existing maps with numbers
 #
-function app-maps
+function app_maps
 {
     # get available maps into MAPS_AVAILABLE
-    get-maps
+    get_maps
+
+    # check that at least one map is available
+    [[ ${#MAPS_AVAILABLE[@]} -lt 1 ]] && \
+        echo "No maps were currently available!" && exit 1
 
     echo "Available maps:"
 
     # print each map with a number (index +1)
     for i in "${!MAPS_AVAILABLE[@]}"; do
-        echo "$(($i + 1)): ${MAPS_AVAILABLE[$i]}"
+        echo "$((i + 1)): ${MAPS_AVAILABLE[$i]}"
     done
 
     echo "NEXT STEP: select a map with './$SCRIPT select <number>'"
@@ -184,15 +214,33 @@ function app-maps
 #
 # select a map (number)
 #
-function app-select
+function app_select
 {
-    true
+    # get available maps into MAPS_AVAILABLE, and count maps
+    get_maps
+    map_count=${#MAPS_AVAILABLE[@]}
+
+    # check at least one map is available
+    [[ $map_count -lt 1 ]] && \
+        echo "No maps were currently available!" && exit 1
+
+    # check for empty or invalid number argument
+    num="$1"
+    [[ ! $num =~ ^[0-9]+$ || $num -lt 1 || $num -gt $map_count ]] && \
+        badUsage "Argument for 'select' has to be an integer in the range 1-$map_count!"
+
+    # select map (convert number to index)
+    map="${MAPS_AVAILABLE[$((num - 1))]}"
+
+    echo -e "selected map:\n$map"
+
+    # get game id from file to global GAME_ID
 }
 
 #
 # enter first room, and save room id to 'data/room_id'
 #
-function app-enter
+function app_enter
 {
     true
 }
@@ -200,7 +248,7 @@ function app-enter
 #
 # show room info of current room
 #
-function app-info
+function app_info
 {
     true
 }
@@ -208,12 +256,16 @@ function app-info
 #
 # enter new room (if possible), and save room id to 'data/room_id'
 #
-function app-go
+function app_go
 {
     true
 }
 
 
+
+##
+## MAIN LOOP
+##
 
 #
 # Main function to process options
@@ -235,13 +287,12 @@ function main
             ( init | maps | select | enter | info | go )
                 command=$1
                 shift
-                app-"$command" "$@"     # run app-function with remaining args
+                app_"$command" "$@"     # run app_function with remaining args
                 exit 0
             ;;
 
             ( * )
                 badUsage "Option/command not recognized."
-                exit 1
             ;;
         esac
     done
@@ -249,6 +300,11 @@ function main
     badUsage
 }
 
+
+
+##
+## ENTRYPOINT
+##
+
 # run main with args
 main "$@"
-exit 1
