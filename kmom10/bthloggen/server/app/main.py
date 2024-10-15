9ffb833669639cdbaa6d6895a6009281838c9604
lohengrin <oljn22@student.bth.se>
2024-10-15 11:36:19 +0200

    Set up the routes in main.py and configure dev env

--- kmom10/bthloggen/server/app/main.py
+++ kmom10/bthloggen/server/app/main.py
@@ -11,10 +11,33 @@ app = FastAPI()
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def get_doc():
+    return {
+        "/": "Supported paths",
+        "/data": "Show all entries",
+        "/data?ip=<ip>": "Show entries with matching ip-adress",
+        "/data?url=<url>": "Show entries with matching url",
+        "/data?month=<month>": "Show entries with matching month",
+        "/data?day=<day>": "Show entries with matching day",
+        "/data?time=<time>": "Show entries with matching time",
+        "/data?ip=<ip>&url=<url>&month=<month>&day=<day>&time=<time>": "Show entries with any combination of matches",
+    }
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/data")
+def get_data(
+    ip: Union[str, None] = None,
+    url: Union[str, None] = None,
+    month: Union[str, None] = None,
+    day: Union[str, None] = None,
+    time: Union[str, None] = None,
+):
+    res = {
+        "ip": ip,
+        "url": url,
+        "month": month,
+        "day": day,
+        "time": time
+    }
+
+    return res
