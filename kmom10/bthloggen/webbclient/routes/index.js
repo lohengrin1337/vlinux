aa6c93a774205582570fa1a311388d270b854010
lohengrin <oljn22@student.bth.se>
2024-10-23 12:18:59 +0200

    Fix style and ux for pagination and search

--- kmom10/bthloggen/webbclient/routes/index.js
+++ kmom10/bthloggen/webbclient/routes/index.js
@@ -2,34 +2,38 @@ const express = require('express');
 const router = express.Router();
 const logModel = require('../models/log_model.js');
 
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'bthlog' });
 });
 
+
+
 /* GET log page. */
 router.get('/data', async function(req, res, next) {
-    const page = parseInt(req.query.page) || 1;
-    const filters = req.query;
+    const filters = {...req.query}; // shallow copy of the query params
     const log = new logModel();
     const entries = await log.getEntries(filters);
     const count = entries.length;
 
-    console.log("*** page: ", page);
-
+    // limit entries to show current page
+    const page = parseInt(req.query.page) || 1;
     const limit = 100;
     const start = (page - 1 ) * limit;
     const end = page * limit;
     const limitedEntries = entries.slice(start, end);
+
+    console.log("*** page: ", page);
 
     const data = {
         title: 'bthlog',
         entries: limitedEntries,
         count: count,
         pageCount: Math.ceil(count / limit),
-        hasMore: count > end,
-        nextPage: page + 1,
-        lastFilters: filters
+        page: page,
+        lastFilters: filters // page: "<num>", and empty filters are removed by logModel
     };
 
     res.render('log', data);
